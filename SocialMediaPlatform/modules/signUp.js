df08a7e2135433adc82c15adac3bf7ebd1564e80
krishna <krishnaasglider@gmail.com>
2026-04-16 11:22:19 +0530

    [Krishna Sharma] updated UI

--- SocialMediaPlatform/modules/signUp.js
+++ SocialMediaPlatform/modules/signUp.js
@@ -1,11 +1,14 @@
 import User from "./user.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 async function createUser(req, res) {
   try {
     const { name, username, email, password } = req.body;
 
-    // ✅ Required fields
     if (!name || !username || !email || !password) {
       return res.render("signUp", { error: "All fields are required" });
     }
@@ -16,35 +19,49 @@ async function createUser(req, res) {
       });
     }
 
-    // ✅ Check email
+
     const isEmail = await User.findOne({ email });
     if (isEmail) {
       return res.render("signUp", { error: "Email already exists" });
     }
 
-    // ✅ Check username
     const isUsername = await User.findOne({ username });
     if (isUsername) {
       return res.render("signUp", { error: "Username already taken" });
     }
-
-    // 🔐 Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // ✅ Save user
-    await User.create({
+    const newUser = await User.create({
       name,
       email,
       username,
       password: hashedPassword
     });
 
-    return res.redirect("/");
+
+    const token = jwt.sign(
+      {
+        id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        username: newUser.username
+      },
+      process.env.TOKENKEY,
+      { expiresIn: "1h" }
+    );
+
+    // 🍪 Set cookie
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: false,     
+      sameSite: "Lax"
+    });
+
+    return res.redirect("/dashboard");
 
   } catch (error) {
     console.error("signUp ERROR:", error);
 
-    // 🔥 VERY IMPORTANT (duplicate index error)
     if (error.code === 11000) {
       if (error.keyPattern?.email) {
         return res.render("signUp", { error: "Email already exists" });
